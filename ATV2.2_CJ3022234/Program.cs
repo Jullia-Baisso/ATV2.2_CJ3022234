62e0bae8ce1a458668e1875ff855ef2e30355fbe
CJ3022234 <CJ3022234@LAB01-PC38>
2024-02-28 10:58:33 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2.2_CJ3022234/Program.cs
@@ -0,0 +1,10 @@
+﻿int n, ant_d, suce_t, soma;
+
+Console.WriteLine("Escreva um número:");
+n= int.Parse(Console.ReadLine());
+
+ant_d = (n * 2) - 1;
+suce_t = (n * 3) + 1;
+soma= ant_d + suce_t;
+
+Console.WriteLine("Soma= {0}", soma);
